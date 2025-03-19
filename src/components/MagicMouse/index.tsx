4a1ca72544d6717ca4403c08d4def17d88631fad
Lincerossa <marcelloluatti@gmail.com>
2025-03-19 11:41:40 +0100

    feat: react-spring dropped in favour of motion

--- src/components/MagicMouse/index.tsx
+++ src/components/MagicMouse/index.tsx
@@ -1,17 +1,12 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { motion } from 'motion/react';
 
 const MagicMouse = () => {
   const [{ clientX: left, clientY: top }, setMousePosition] = useState({
     clientX: 0,
     clientY: 0,
-  });
-
-  const styles = useSpring({
-    from: { left: 0, top: 0 },
-    to: { left, top },
   });
 
   useEffect(() => {
@@ -28,7 +23,10 @@ const MagicMouse = () => {
         }}
         className={`bg-[#3498db] -translate-x-1/2 -translate-y-1/2 fixed h-3 w-3 rounded-full pointer-events-none z-3`}
       />
-      <animated.div
+      <motion.div
+        initial={{ left: 0, top: 0 }}
+        animate={{ left, top }}
+        transition={{ duration: 0.2 }}
         style={{
           position: 'fixed',
           zIndex: 3,
@@ -38,7 +36,6 @@ const MagicMouse = () => {
           transform: 'translate(-50%, -50%)',
           width: '22px',
           height: '22px',
-          ...styles,
         }}
       />
     </>
