d0960f03acbad0eced9d19e48abdd0c345bb58f7
Lincerossa <marcelloluatti@gmail.com>
2025-03-22 12:49:05 +0100

    feat: finalized fs sync

--- src/containers/Post.tsx
+++ src/containers/Post.tsx
@@ -1,18 +1,25 @@
+import { FC } from 'react';
+import ReactMarkdown from 'react-markdown';
 import PageHero from '@/components/PageHero';
 import Wrapper from '@/components/Wrapper';
-import { FC } from 'react';
 
 type PostProps = {
   title: string;
   description: string;
+  content: string;
 };
 
-const Post: FC<PostProps> = ({ title, description }) => {
+const Post: FC<PostProps> = ({ title, description, content }) => {
   return (
     <>
       <PageHero title={title} />
       <Wrapper>
         <div className="py-8 text-xl tracking-wide">{description}</div>
+        <div className="blog-section py-8 text-xl tracking-wide">
+          <ReactMarkdown urlTransform={(uri) => uri.replace('public', '')}>
+            {content}
+          </ReactMarkdown>
+        </div>
       </Wrapper>
     </>
   );
